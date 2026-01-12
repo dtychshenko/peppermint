96fa26eb806557ddf7aa8db7c6e3214c9c1faad3
Dmitriy T <dtychshenko@hotmail.com>
2026-01-11 22:07:32 -0500

    feat: add logos and more info

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -14,7 +14,8 @@ function HomepageHeader() {
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          <img src="/img/logo-inverted.svg" alt="Peppermint logo" style={{ height: "0.75em" }} />
+          &nbsp;{siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
