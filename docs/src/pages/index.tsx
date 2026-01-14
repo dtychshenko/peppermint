04abae0d5ce6f924a7f7c072caf78b6efd481e1d
Dmitriy T <dtychshenko@hotmail.com>
2026-01-14 11:52:05 -0500

    chore: update tagline

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -36,9 +36,7 @@ function HomepageHeader() {
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description={siteConfig.tagline}>
       <main>
         <HomepageHeader />
       </main>
