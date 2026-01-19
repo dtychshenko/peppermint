e5acc75177b78ad13afbc82e764e570988a248a1
Dmitriy T <dtychshenko@hotmail.com>
2026-01-19 11:05:44 -0500

    fix: adjust language selector

--- src/app/layouts/header/parts/LanguageMenu.tsx
+++ src/app/layouts/header/parts/LanguageMenu.tsx
@@ -5,13 +5,13 @@ export function LanguageMenu() {
   return (
     <Menu>
       <Menu.Target>
-        <ActionIcon variant="default" aria-label="Language selector" color="blue.6">
+        <ActionIcon variant="subtle" aria-label="Language selector" color="dark.3">
           <IconLanguage stroke={1.5} />
         </ActionIcon>
       </Menu.Target>
       <Menu.Dropdown>
-        <Menu.Item>English</Menu.Item>
-        <Menu.Item>Français</Menu.Item>
+        <Menu.Item color="dark.3">English</Menu.Item>
+        <Menu.Item color="dark.3">Français</Menu.Item>
       </Menu.Dropdown>
     </Menu>
   );
