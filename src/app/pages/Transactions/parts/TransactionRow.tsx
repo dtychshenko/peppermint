06da555b8105f84be49c9d1e5d031c6bd6907f54
Dmitriy T <dtychshenko@hotmail.com>
2026-01-20 21:24:55 -0500

    feat: add row selections

--- src/app/pages/Transactions/parts/TransactionRow.tsx
+++ src/app/pages/Transactions/parts/TransactionRow.tsx
@@ -1,27 +1,34 @@
-import { Table, TextInput } from "@mantine/core";
+import { Checkbox, Table, TextInput } from "@mantine/core";
 import { DatePickerInput } from "@mantine/dates";
-import { memo } from "react";
+import { memo, useCallback, useMemo } from "react";
 import { TransactionDAO } from "../../../db";
 import { toDisplayCurrency } from "../../../shared/numbers";
 
 interface Props {
   transaction: TransactionDAO;
+  selected: boolean;
+  onSelect: (transactionId: string) => void;
 }
 
-export const TransactionRow = memo(function TransactionRow({ transaction }: Props) {
+export const TransactionRow = memo(function TransactionRow(props: Props) {
+  const { transaction, selected, onSelect } = props;
+
+  const onClick = useCallback(() => onSelect(transaction.id), [onSelect, transaction.id]);
+  const amount = useMemo(() => toDisplayCurrency(transaction.amount), [transaction.amount]);
+  const date = useMemo(() => new Date(transaction.date), [transaction.date]);
+
   return (
     <>
+      <Table.Td w={40}>
+        <Checkbox checked={selected} onChange={onClick} />
+      </Table.Td>
       <Table.Td w={110}>
-        <DatePickerInput
-          value={new Date(transaction.date)}
-          variant="unstyled"
-          valueFormat="ddd, MMM D"
-        />
+        <DatePickerInput value={date} variant="unstyled" valueFormat="ddd, MMM D" />
       </Table.Td>
       <Table.Td>
         <TextInput variant="unstyled" placeholder="Add a payee" defaultValue={transaction.payee} />
       </Table.Td>
-      <Table.Td align="right">{toDisplayCurrency(transaction.amount)}</Table.Td>
+      <Table.Td align="right">{amount}</Table.Td>
     </>
   );
 });
--- src/app/pages/Transactions/parts/TransactionsTable.tsx
+++ src/app/pages/Transactions/parts/TransactionsTable.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Table } from "@mantine/core";
-import { useMemo } from "react";
+import { Checkbox, Table } from "@mantine/core";
+import { useCallback, useState } from "react";
 import { type TableProps, TableVirtuoso } from "react-virtuoso";
 import { MAIN_HEADER_HEIGHT } from "../../../config/layout";
 import { TransactionDAO } from "../../../db";
@@ -11,32 +11,56 @@ interface Props {
   transactions: Array<TransactionDAO>;
 }
 
-const Headers = () => (
-  <Table.Tr>
-    <Table.Th>Date</Table.Th>
-    <Table.Th>Payee</Table.Th>
-    <Table.Th align="right">Amount</Table.Th>
-  </Table.Tr>
-);
+const components = {
+  Table: (props: TableProps) => (
+    <Table
+      {...props}
+      highlightOnHover
+      striped
+      stickyHeader
+      stickyHeaderOffset={MAIN_HEADER_HEIGHT}
+      style={{ ...props.style, borderCollapse: "separate" }}
+    />
+  ),
+  TableHead: Table.Thead,
+  TableRow: Table.Tr,
+  TableBody: Table.Tbody,
+};
 
 export function TransactionsTable({ transactions }: Props) {
-  const components = useMemo(
-    () => ({
-      Table: (props: TableProps) => (
-        <Table
-          {...props}
-          highlightOnHover
-          striped
-          stickyHeader
-          stickyHeaderOffset={MAIN_HEADER_HEIGHT}
-          style={{ ...props.style, borderCollapse: "separate" }}
-        />
-      ),
-      TableHead: Table.Thead,
-      TableRow: Table.Tr,
-      TableBody: Table.Tbody,
-    }),
-    [],
+  const [selected, setSelected] = useState<Set<string>>(new Set());
+
+  const onSelect = useCallback((transactionId: string) => {
+    setSelected((prev) => {
+      const newSelected = new Set(prev);
+
+      if (newSelected.has(transactionId)) {
+        newSelected.delete(transactionId);
+      } else {
+        newSelected.add(transactionId);
+      }
+
+      return newSelected;
+    });
+  }, []);
+
+  const Headers = useCallback(
+    () => (
+      <Table.Tr>
+        <Table.Th w={40}>
+          <Checkbox
+            checked={selected.size === transactions.length}
+            onChange={({ currentTarget: { checked } }) => {
+              setSelected(checked ? new Set(transactions.map(({ id }) => id)) : new Set());
+            }}
+          />
+        </Table.Th>
+        <Table.Th>Date</Table.Th>
+        <Table.Th>Payee</Table.Th>
+        <Table.Th align="right">Amount</Table.Th>
+      </Table.Tr>
+    ),
+    [selected.size, transactions.length],
   );
 
   return (
@@ -49,7 +73,13 @@ export function TransactionsTable({ transactions }: Props) {
       components={components}
       fixedHeaderContent={Headers}
       computeItemKey={(_index, item) => item.id}
-      itemContent={(_index, transaction) => <TransactionRow transaction={transaction} />}
+      itemContent={(_index, transaction) => (
+        <TransactionRow
+          transaction={transaction}
+          selected={selected.has(transaction.id)}
+          onSelect={onSelect}
+        />
+      )}
     />
   );
 }
